1890da49a30f871320b4ca8631fd48df411578b6
Jan Luca Naumann <j.naumann@fu-berlin.de>
2021-09-21 10:56:49 +0200

    Use random numbers from interval [-1, 1] instead of just postive numbers

--- peps_ad/utils/random.py
+++ peps_ad/utils/random.py
@@ -114,11 +114,11 @@ class PEPS_Numpy_Random(PEPS_Random_Impl):
             or np.dtype(dtype) is np.dtype(np.complex128)
             or np.dtype(dtype) is np.dtype(np.complex256)
         ):
-            block = self.rng.random(dim).astype(dtype) + 1j * self.rng.random(
-                dim
+            block = self.rng.uniform(-1, 1, dim).astype(dtype) + 1j * self.rng.uniform(
+                -1, 1, dim
             ).astype(dtype)
         else:
-            block = self.rng.random(dim, dtype=dtype) # type: ignore
+            block = self.rng.uniform(-1, 1, dim, dtype=dtype)  # type: ignore
         if normalize:
             block /= np.linalg.norm(block)
         return block
@@ -141,17 +141,16 @@ class PEPS_Jax_Random(PEPS_Random_Impl):
     def block(
         self, dim: Sequence[int], dtype: Type[np.number], *, normalize: bool = True
     ) -> jnp.ndarray:
-        if (
-            jnp.dtype(dtype) is jnp.dtype(jnp.complex64)
-            or jnp.dtype(dtype) is jnp.dtype(jnp.complex128)
-        ):
+        if jnp.dtype(dtype) is jnp.dtype(jnp.complex64) or jnp.dtype(
+            dtype
+        ) is jnp.dtype(jnp.complex128):
             self.key, key1, key2 = jax.random.split(self.key, 3)
-            block = jax.random.uniform(key1, dim).astype(
+            block = jax.random.uniform(key1, dim, minval=-1, maxval=1).astype(
                 dtype
-            ) + 1j * jax.random.uniform(key2, dim).astype(dtype)
+            ) + 1j * jax.random.uniform(key2, dim, minval=-1, maxval=1).astype(dtype)
         else:
             self.key, key1 = jax.random.split(self.key, 2)
-            block = jax.random.uniform(key1, dim, dtype=dtype)
+            block = jax.random.uniform(key1, dim, dtype=dtype, minval=-1, maxval=1)
         if normalize:
             block /= jnp.linalg.norm(block)
         return block
