1d99857690f75338ed52d66190cf28173fe6d742
Jan Luca Naumann <janluca@zedat.fu-berlin.de>
2021-10-13 17:54:03 +0200

    Format the sphinx conf.py by black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'peps-ad'
-copyright = '2021, Philipp Schmoll, Augustine Kshetrimayum, Frederik Wilde, Jan Naumann'
-author = 'Philipp Schmoll, Augustine Kshetrimayum, Frederik Wilde, Jan Naumann'
+project = "peps-ad"
+copyright = "2021, Philipp Schmoll, Augustine Kshetrimayum, Frederik Wilde, Jan Naumann"
+author = "Philipp Schmoll, Augustine Kshetrimayum, Frederik Wilde, Jan Naumann"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,11 +32,11 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
 ]
 
 napoleon_include_private_with_doc = True
@@ -48,16 +49,16 @@ autodoc_type_aliases = {
     "jax._src.numpy.lax_numpy.ndarray": "jax.numpy.ndarray",
 }
 
-#autodoc_mock_imports = ["jax"]
+# autodoc_mock_imports = ["jax"]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3', None),
-    'jax': ('https://jax.readthedocs.io/en/latest/', None),
-    'numpy': ('https://numpy.org/doc/stable/', None),
+    "python": ("https://docs.python.org/3", None),
+    "jax": ("https://jax.readthedocs.io/en/latest/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -70,9 +71,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
