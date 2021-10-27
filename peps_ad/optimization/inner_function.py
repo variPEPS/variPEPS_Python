e26648c2cbf4a052652e4209ed88f76eec1e6c44
Jan Luca Naumann <janluca@zedat.fu-berlin.de>
2021-10-27 16:09:07 +0200

    Add function to calculate gradient and value of CTMRG with preconverged env

--- peps_ad/optimization/inner_function.py
+++ peps_ad/optimization/inner_function.py
@@ -24,3 +24,34 @@ def calc_ctmrg_expectation(
 
 
 calc_ctmrg_expectation_value_and_grad = value_and_grad(calc_ctmrg_expectation, has_aux=True)
+
+
+def calc_preconverged_ctmrg_value_and_grad(
+    peps_tensors: Sequence[jnp.ndarray],
+    unitcell: PEPS_Unit_Cell,
+    expectation_func: Expectation_Model,
+    *,
+    preconverged_eps: float = 1e-5,
+    final_eps: float = 1e-8,
+    preconverged_max_steps: int = 100,
+    final_max_steps: int = 10,
+) -> jnp.ndarray:
+    preconverged_unitcell, preconverged_corner_svds = calc_ctmrg_env(
+        peps_tensors, unitcell, eps=preconverged_eps, max_steps=preconverged_max_steps
+    )
+
+    (
+        expectation_value,
+        (final_unitcell, final_corner_svds),
+    ), gradient = calc_ctmrg_expectation_value_and_grad(
+        peps_tensors,
+        preconverged_unitcell,
+        expectation_func,
+        eps=final_eps,
+        max_steps=final_max_steps,
+    )
+
+    return (
+        expectation_value,
+        (final_unitcell, preconverged_corner_svds, final_corner_svds),
+    ), gradient
