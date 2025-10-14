4cc7fbc24fa3891b9af6bb07763c36b7c5a9d792
Marc-Philipp Tillschneider <m-p.tillschneider@tum.de>
2025-10-14 16:17:55 +0200

    Added a docString to optimize_peps_network to explan functionality

--- varipeps/optimization/optimizer.py
+++ varipeps/optimization/optimizer.py
@@ -463,6 +463,11 @@ def optimize_peps_network(
 
     As convergence criterion the norm of the gradient is used.
 
+    If the very first CTMRG calculation does not converge,
+    a object of type :obj:`scipy.optimize.OptimizeResult`
+    is returned with Success=False. This case should be
+    handled by the scriptcalling this function.
+
     Args:
       input_tensors (:obj:`~varipeps.peps.PEPS_Unit_Cell` or :term:`sequence` of :obj:`jax.numpy.ndarray`):
         The PEPS unitcell to work on or the tensors which should be mapped by
