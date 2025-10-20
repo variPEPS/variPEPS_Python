ad0d1bae7ac457ae8d34697ebf29831d4fcaf40d
Marc-Philipp Tillschneider <m-p.tillschneider@tum.de>
2025-10-20 09:34:33 +0200

    another fix for the logger

--- varipeps/optimization/optimizer.py
+++ varipeps/optimization/optimizer.py
@@ -937,8 +937,6 @@ def optimize_peps_network(
                                 additional_input,
                             )
 
-                        pbar.reset()
-                        pbar.refresh()
 
                         continue
                     else:
