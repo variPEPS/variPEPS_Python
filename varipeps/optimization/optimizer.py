fbc6a4ae274be86032dd54cb7f6a6a3247781dae
Marc-Philipp Tillschneider <m-p.tillschneider@tum.de>
2025-10-23 20:40:18 +0200

    Add delta to logging

--- varipeps/optimization/optimizer.py
+++ varipeps/optimization/optimizer.py
@@ -1162,9 +1162,10 @@ def optimize_peps_network(
 
         step_str = f"{float(linesearch_step):0.8f}" if linesearch_step is not None else "None"
         logger.info(
-            "📉 Step %d | Energy: %.8f | Retries: %d | Conv: %.3e | Line search step: %s | Max. trunc. err.: %.3e | Runtime: %.0f sec",
+            "📉 Step %d | Energy: %.8f Δ: %.2e | Retries: %d | Conv: %.3e | Line search step: %s | Max. trunc. err.: %.3e | Runtime: %.0f sec",
             int(count),
             float(working_value),
+            -float(working_value - step_energies[random_noise_retries][-2]) if len(step_energies[random_noise_retries]) > 1 else 0.0,
             int(random_noise_retries),
             float(conv),
             step_str,
